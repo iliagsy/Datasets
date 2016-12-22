ac8ef25df22a5da040ff197871a47bbfd5c1c0f5
gaoshiyu <gaoshiyu@intern.douban.com>
2016-12-22 14:21:24 +0800

    init keep top

--- code/keep_top.py
+++ code/keep_top.py
@@ -6,14 +6,13 @@ from consts import GRAPH_FILE_GZ, SPECIES, FINAL_FILE
 def keep_top(species, d=5, savefn=None):
     PM = loadtxt(GRAPH_FILE_GZ[species])  # Pearson Matrix
     for i in range(PM.shape[0]):
-        kv_lst = sorted(
-            sorted(
-                zip(range(len(PM[i])), PM[i].tolist()),
-                key=lambda t: t[1],
-                reverse=True
-            )[:d],
-            key=lambda t: t[0]
+        kv_lst_ = sorted(
+            zip(range(len(PM[i])), PM[i].tolist()),
+            key=lambda t: t[1],
+            reverse=True
         )
+        kv_lst_[d:] = 0
+        kv_lst = sorted(kv_lst_, key=lambda t: t[0])
         val_lst = [t[1] for t in kv_lst]
         PM[i] = array(val_lst)
     if savefn:
