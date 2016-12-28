810025f76bc22d8f1665bc0534dca1435ef9bc83
gaoshiyu <gaoshiyu@intern.douban.com>
2016-12-29 02:15:43 +0800

    +

--- code/keep_top.py
+++ code/keep_top.py
@@ -12,15 +12,9 @@ def keep_top(species, PM=None, d=5, savefn=None, ret_arr=True):
     # PM_f: PM filtered -- bool type
     PM_f = ndarray(shape=PM.shape, dtype=bool)
     for i in range(PM.shape[0]):
-        kv_arr_ = array(sorted(
-            zip(range(len(PM[i])), PM[i].tolist()),
-            key=lambda t: t[1],
-            reverse=True
-        ))
-        kv_arr_[d:, 1] = 0
-        kv_arr = array(sorted(kv_arr_.tolist(), key=lambda t: t[0]))
-        val_arr = kv_arr[:, 1]
-        PM_f[i] = (val_arr != 0)
+        sidx = PM[i].argsort()[::-1]  # sorted index array
+        PM_f[i, sidx[:d]] = True
+        PM_f[i, sidx[d:]] = False
     # PM_b: PM binary, i.e. complemented PM_f
     PM_b = logical_or(PM_f.transpose(), PM_f)
     nE = sum(PM_b) / 2; nV = PM.shape[0]
@@ -35,7 +29,7 @@ def keep_top(species, PM=None, d=5, savefn=None, ret_arr=True):
         return res
     # calc new PM, with PM_b as condition
     # numpy.where(condition[, x, y]) -- Return elements, either from x or y, depending on condition.
-    PM = where(PM_b, PM, zeros_like(PM))
+    PM = where(PM_b, PM, zeros_like(PM))  # array-wise `c ? a : b`
     # wrap up
     if savefn:
         savetxt(savefn, PM)
@@ -48,7 +42,7 @@ for d in (5, 8, 10):
         res_l = []
         data = keep_top(sp, d=d, ret_arr=False)
         data = map(data.get, ['d','species','nE','nV','nE_per_V'])
-        s = '%4d%10s%8d%8d%8.4f' % tuple(data)
+        s = '%4d%10s%8d%8d%8.4f' % tuple(data)  # 不可以是list
         res_l.append(s)
     with open(GRAPH_PRF, 'w') as fh:
         fh.write('\n'.join(res_l))
